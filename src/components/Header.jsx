4276ff6e326874cd2960affdb7cd32e653639a9c
Joel Pitzer <jpitzer5@gmail.com>
2025-01-16 15:18:39 -0800

    set up project and made header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return(
+        <header>
+            <img src="/src/assets/globe.png" alt="globe" />
+            <span>
+                my travel journal.
+            </span>
+        </header>
+    )
+}
+
+// 
